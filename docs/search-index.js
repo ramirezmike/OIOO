4180da9677ecf960b09c65a2779da38a5ffba5c3
mramirez <ramirezmike2@gmail.com>
2020-06-19 10:23:36 -0400

    added generated docs

--- docs/search-index.js
+++
@@ -1,3 +0,0 @@
-var searchIndex={};
-searchIndex["lib"] = {"doc":"","i":[],"p":[]};
-addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ target/doc/aliases.js
@@ -0,0 +1,9 @@
+var ALIASES = {};
+ALIASES["cfg_if"] = {};
+ALIASES["getrandom"] = {};
+ALIASES["libc"] = {};
+ALIASES["oioo"] = {};
+ALIASES["ppv_lite86"] = {};
+ALIASES["rand"] = {};
+ALIASES["rand_chacha"] = {};
+ALIASES["rand_core"] = {};
